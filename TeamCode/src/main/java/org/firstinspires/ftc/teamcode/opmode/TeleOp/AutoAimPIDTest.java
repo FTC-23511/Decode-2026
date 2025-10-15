75578474e9d733e994a81d3ae577b0098f7e3053
AjayPlus <ajayraghavan@live.com>
2025-10-14 17:06:34 -0700

    implemented math for hood and shooter angle
    set some constants to 0.00 like velocity cause idk what they should be

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TeleOp/AutoAimPIDTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TeleOp/AutoAimPIDTest.java
@@ -1,5 +1,15 @@
 package org.firstinspires.ftc.teamcode.opmode.TeleOp;
+import static org.firstinspires.ftc.teamcode.globals.Constants.BLUE_TARGET_X;
+import static org.firstinspires.ftc.teamcode.globals.Constants.BLUE_TARGET_Y;
+import static org.firstinspires.ftc.teamcode.globals.Constants.EXIT_VELO;
+import static org.firstinspires.ftc.teamcode.globals.Constants.G;
 import static org.firstinspires.ftc.teamcode.globals.Constants.HOOD_SET_POSITION_TUNER;
+import static org.firstinspires.ftc.teamcode.globals.Constants.H_TARGET;
+import static org.firstinspires.ftc.teamcode.globals.Constants.RED_TARGET_X;
+import static org.firstinspires.ftc.teamcode.globals.Constants.RED_TARGET_Y;
+import static org.firstinspires.ftc.teamcode.globals.Constants.SHOOTER_DX;
+import static org.firstinspires.ftc.teamcode.globals.Constants.SHOOTER_DY;
+import static org.firstinspires.ftc.teamcode.globals.Constants.SHOOTER_RELEASE_HEIGHT;
 
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
@@ -11,6 +21,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.seattlesolvers.solverslib.command.CommandScheduler;
 import com.seattlesolvers.solverslib.gamepad.GamepadEx;
+import com.seattlesolvers.solverslib.geometry.Pose2d;
+import com.seattlesolvers.solverslib.geometry.Rotation2d;
+import com.seattlesolvers.solverslib.geometry.Transform2d;
+import com.seattlesolvers.solverslib.geometry.Translation2d;
 import com.seattlesolvers.solverslib.hardware.motors.CRServo;
 import com.seattlesolvers.solverslib.hardware.motors.CRServoEx;
 import com.seattlesolvers.solverslib.util.TelemetryData;
@@ -27,6 +41,7 @@ public class AutoAimPIDTest extends OpMode {
     public GamepadEx operator;
 
     public ElapsedTime timer;
+
 
     TelemetryData telemetryData = new TelemetryData(telemetry);
 
@@ -61,6 +76,52 @@ public class AutoAimPIDTest extends OpMode {
 
     @Override
     public void loop() {
+
+        Pose2d robotPose = robot.drive.getPose();
+        Transform2d shooterOffset = new Transform2d(new Translation2d(SHOOTER_DX, SHOOTER_DY), new Rotation2d(0.0));
+        Pose2d shooterPose = robotPose.plus(shooterOffset);
+
+        double targetX, targetY;
+        if (Constants.ALLIANCE_COLOR == Constants.AllianceColor.BLUE) {
+            targetX = BLUE_TARGET_X;
+            targetY = BLUE_TARGET_Y;
+        } else {
+            targetX = RED_TARGET_X;
+            targetY = RED_TARGET_Y;
+        }
+
+        double dx = targetX - shooterPose.getX();
+        double dy = targetY - shooterPose.getY();
+        double d  = Math.hypot(dx, dy);
+        double dH = H_TARGET - SHOOTER_RELEASE_HEIGHT;
+
+        double desiredHeading = Math.atan2(dy, dx);
+        double robotHeading   = robotPose.getRotation().getRadians();
+        double delta          = desiredHeading - robotHeading;
+        double turretYawRad   = Math.atan2(Math.sin(delta), Math.cos(delta));
+
+        double v2 = EXIT_VELO * EXIT_VELO;
+        double discriminant = v2*v2 - G * (G*d*d + 2.0*dH*v2);
+
+        Double hoodLowRad = null, hoodHighRad = null;
+        if (discriminant >= 0.0 && d > 1e-6) {
+            double root = Math.sqrt(discriminant);
+            double tanHigh = (v2 + root) / (G*d);
+            double tanLow  = (v2 - root) / (G*d);
+            hoodHighRad = Math.atan(tanHigh);
+            hoodLowRad  = Math.atan(tanLow);
+        }
+
+        telemetryData.addData("TurretYaw (rad)", turretYawRad);
+        if (hoodLowRad != null) {
+            telemetryData.addData("HoodLow (rad)", hoodLowRad);
+            telemetryData.addData("HoodHigh (rad)", hoodHighRad);
+        } else {
+            telemetryData.addData("Hood", "No Solution");
+        }
+        telemetryData.update();
+
+
         LLStatus status = limelight.getStatus();
         telemetry.addData("Name", "%s",
                 status.getName());
@@ -68,6 +129,7 @@ public class AutoAimPIDTest extends OpMode {
                 status.getTemp(), status.getCpu(), (int) status.getFps());
         telemetry.addData("Pipeline", "Index: %d, Type: %s",
                 status.getPipelineIndex(), status.getPipelineType());
+        telemetryData.update();
 
         LLResult result = limelight.getLatestResult();
         if (result != null && result.isValid()) {
@@ -79,6 +141,8 @@ public class AutoAimPIDTest extends OpMode {
                 double tyDegrees = fiducial.getTargetYDegrees();
                 double tyPixels = fiducial.getTargetYPixels();
                 double txPixels = fiducial.getTargetXPixels();
+
+
 
                 // Blue ID = 20
                 // Obelisk PPG ID = 21
@@ -103,10 +167,13 @@ public class AutoAimPIDTest extends OpMode {
                     }
 
 
+
+
                     telemetryData.addData("txPixels", txPixels);
                     telemetryData.addData("tyPixels", tyPixels);
                     telemetryData.addData("txDegrees", txDegrees);
                     telemetryData.addData("tyDegrees", tyDegrees);
+                    telemetryData.update();
                 }
             }
         }
