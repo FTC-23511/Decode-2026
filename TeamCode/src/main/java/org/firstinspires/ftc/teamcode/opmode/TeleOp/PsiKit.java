eef96bbf8535c70e27465304755f73a32b406506
eyz11 <141088250+eyz11@users.noreply.github.com>
2025-10-24 19:58:36 -0700

    psikit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TeleOp/PsiKit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TeleOp/PsiKit.java
@@ -3,9 +3,11 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.seattlesolvers.solverslib.command.CommandScheduler;
 import com.seattlesolvers.solverslib.command.InstantCommand;
+import com.seattlesolvers.solverslib.drivebase.swerve.coaxial.CoaxialSwerveModule;
 import com.seattlesolvers.solverslib.gamepad.GamepadEx;
 import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
 import com.seattlesolvers.solverslib.geometry.Pose2d;
+import com.seattlesolvers.solverslib.kinematics.wpilibkinematics.ChassisSpeeds;
 import com.seattlesolvers.solverslib.util.TelemetryData;
 
 import org.firstinspires.ftc.teamcode.globals.Constants;
@@ -23,7 +25,7 @@ public class PsiKit extends PsiKitOpMode{
         public ElapsedTime timer;
         TelemetryData telemetryData = new TelemetryData(telemetry);
 
-    private final Robot robot = Robot.getInstance();
+        private final Robot robot = Robot.getInstance();
         public void initialize() {
             // Must have for all opModes
             Constants.OP_MODE_TYPE = Constants.OpModeType.TELEOP;
@@ -67,6 +69,23 @@ public class PsiKit extends PsiKitOpMode{
           Init logic goes here
 
          */
+                // Must have for all opModes
+                Constants.OP_MODE_TYPE = Constants.OpModeType.TELEOP;
+
+                // Resets the command scheduler
+                CommandScheduler.getInstance().reset();
+
+                // Initialize the robot (which also registers subsystems, configures CommandScheduler, etc.)
+                robot.init(hardwareMap);
+
+                driver = new GamepadEx(gamepad1);
+                operator = new GamepadEx(gamepad2);
+
+                // Driver controls
+                // Reset heading
+                driver.getGamepadButton(GamepadKeys.Button.DPAD_UP).whenPressed(
+                        new InstantCommand(() -> robot.drive.setPose(new Pose2d()))
+                );
 
                 Logger.periodicAfterUser(0.0, 0.0);
                 // logging these timestamps is completely optional
@@ -88,6 +107,45 @@ public class PsiKit extends PsiKitOpMode{
           OpMode logic goes here
 
          */
+                // Keep all the has movement init for until when TeleOp starts
+                // This is like the init but when the program is actually started
+                if (timer == null) {
+                    robot.initHasMovement();
+                    timer = new ElapsedTime();
+                }
+
+                // Update any constants that are being updated by FTCDash
+                for (CoaxialSwerveModule module : robot.drive.swerve.getModules()) {
+                    module.setSwervoPIDF(Constants.SWERVO_PIDF_COEFFICIENTS);
+                }
+
+                // Drive the robot
+                double minSpeed = 0.3; // As a fraction of the max speed of the robot
+                double speedMultiplier = minSpeed + (1 - minSpeed) * driver.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER);
+                robot.drive.swerve.updateWithTargetVelocity(
+                        ChassisSpeeds.fromFieldRelativeSpeeds(
+                                driver.getLeftY() * Constants.MAX_VELOCITY * speedMultiplier,
+                                -driver.getLeftX() * Constants.MAX_VELOCITY * speedMultiplier,
+                                -driver.getRightX() * Constants.MAX_ANGULAR_VELOCITY * speedMultiplier,
+                                robot.drive.getPose().getRotation()
+                        )
+                );
+
+                telemetryData.addData("Loop Time", timer.milliseconds());
+                timer.reset();
+
+                telemetryData.addData("Heading", robot.drive.getPose().getHeading());
+                telemetryData.addData("Robot Pose", robot.drive.getPose());
+
+                telemetryData.addData("Target Chassis Velocity", robot.drive.swerve.getTargetVelocity());
+                telemetryData.addData("FR Module", robot.drive.swerve.getModules()[0].getTargetVelocity() + " | " + robot.drive.swerve.getModules()[0].getPowerTelemetry());
+                telemetryData.addData("FL Module", robot.drive.swerve.getModules()[1].getTargetVelocity() + " | " + robot.drive.swerve.getModules()[1].getPowerTelemetry());
+                telemetryData.addData("BL Module", robot.drive.swerve.getModules()[2].getTargetVelocity() + " | " + robot.drive.swerve.getModules()[2].getPowerTelemetry());
+                telemetryData.addData("BR Module", robot.drive.swerve.getModules()[3].getTargetVelocity() + " | " + robot.drive.swerve.getModules()[3].getPowerTelemetry());
+
+                // DO NOT REMOVE ANY LINES BELOW! Runs the command scheduler and updates telemetry
+                CommandScheduler.getInstance().reset();
+                telemetryData.update();
 
                 Logger.recordOutput("OpMode/example", 2.0);
                 // example
@@ -100,6 +158,15 @@ public class PsiKit extends PsiKitOpMode{
                 );
                 // alternately, keep track of how long some things are taking. up to
                 // you on what you want to do
+
+                Logger.recordOutput("Drive/Loop Time", timer.milliseconds());
+                Logger.recordOutput("Drive/Heading", robot.drive.getPose().getHeading());
+                Logger.recordOutput("Drive/RobotPose",robot.drive.getPose().toString());
+                Logger.recordOutput("Drive/TargetChassisVelocity/Velocity",robot.drive.swerve.getTargetVelocity().toString());
+                Logger.recordOutput("Drive/FRModule/Velocity",robot.drive.swerve.getModules()[0].getTargetVelocity().toString());
+                Logger.recordOutput("Drive/FLModule/Velocity",robot.drive.swerve.getModules()[0].getTargetVelocity().toString());
+                Logger.recordOutput("Drive/BRModule/Velocity",robot.drive.swerve.getModules()[0].getTargetVelocity().toString());
+                Logger.recordOutput("Drive/FLModule/Velocity",robot.drive.swerve.getModules()[0].getTargetVelocity().toString());
             }
             Logger.end();
         }
