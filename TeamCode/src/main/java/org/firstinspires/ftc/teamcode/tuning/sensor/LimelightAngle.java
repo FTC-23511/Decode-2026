ad3020230bfd517f971b8239231388b1845ab5ab
ArushYadlapati <arush.yadlapati@gmail.com>
2025-11-12 21:20:32 -0800

    Add degrees to telemetry view

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/sensor/LimelightAngle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/sensor/LimelightAngle.java
@@ -69,7 +69,8 @@ public class LimelightAngle extends CommandOpMode {
 
         telemetryData.addData("loop time", timer.milliseconds());
         timer.reset();
-        telemetryData.addData("Angle", robot.turret.tyOffset(lastKnownPose));
+        telemetryData.addData("Angle (Radians)", robot.turret.tyOffset(lastKnownPose));
+        telemetryData.addData("Angle (Degrees)", Math.toDegrees(robot.turret.tyOffset(lastKnownPose)));
         telemetryData.addData("bot pose", lastKnownPose);
 
 
