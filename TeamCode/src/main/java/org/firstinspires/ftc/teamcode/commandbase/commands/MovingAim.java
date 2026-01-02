87f90878f367a5a2cc00b61d1ed2d014b4545986
eyz11 <141088250+eyz11@users.noreply.github.com>
2026-01-01 21:33:55 -0800

    updated movingaim

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandbase/commands/MovingAim.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandbase/commands/MovingAim.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.commandbase.commands;
 import static org.firstinspires.ftc.teamcode.commandbase.subsystems.Turret.TurretState.ANGLE_CONTROL;
 import static org.firstinspires.ftc.teamcode.commandbase.subsystems.Turret.TurretState.GOAL_LOCK_CONTROL;
 import static org.firstinspires.ftc.teamcode.globals.Constants.AIMBOT_COEFFICIENTS;
+import static org.firstinspires.ftc.teamcode.globals.Constants.DISTANCE_UNIT;
 import static org.firstinspires.ftc.teamcode.globals.Constants.GOAL_POSE;
 import static org.firstinspires.ftc.teamcode.globals.Constants.LAUNCHER_CLOSE_VELOCITY;
 import static org.firstinspires.ftc.teamcode.globals.Constants.LAUNCHER_HEIGHT;
@@ -27,19 +28,17 @@ public class MovingAim extends CommandBase {
     Robot robot;
     final double inchesPerMeters = 39.3701;
     final double BALL_RADIUS = 2.5;
-    private final ElapsedTime timer;
-    Position target;
 
     MathFunctions.ShootingMath math;
+
     public MovingAim() {
         robot = Robot.getInstance();
-        timer = new ElapsedTime();
 
         robot.readyToLaunch = false;
 
         final Pose2d goalPose = GOAL_POSE();
-        Position goalPosition = new Position(DistanceUnit.METER, goalPose.getX() * inchesPerMeters, goalPose.getY() * inchesPerMeters, TARGET_HEIGHT, 0);
-        math = new MathFunctions.ShootingMath(goalPosition, LAUNCHER_HEIGHT, BALL_RADIUS);
+        Position goalPosition = new Position(DISTANCE_UNIT, goalPose.getX(), goalPose.getY(), TARGET_HEIGHT * inchesPerMeters, 0);
+        math = new MathFunctions.ShootingMath(goalPosition, LAUNCHER_HEIGHT * inchesPerMeters, BALL_RADIUS);
 
         addRequirements(robot.launcher, robot.turret, robot.drive, robot.intake);
     }
@@ -56,18 +55,18 @@ public class MovingAim extends CommandBase {
         if (!Turret.turretState.equals(GOAL_LOCK_CONTROL)) {
             robot.turret.setTurret(GOAL_LOCK_CONTROL, 0);
         }
-
-        timer.reset();
     }
+
     public void execute() {
         predictSet();
-            if(robot.launcher.flywheelReady()&&robot.turret.readyToLaunch()) {
-                robot.readyToLaunch = true;
-            }
+        // TODO: to add the turrentReady() function.
+        if (robot.launcher.flywheelReady() && robot.turret.readyToLaunch()) {
+            robot.readyToLaunch = true;
+        }
 
     }
 
-    public void end(boolean interrupted){
+    public void end(boolean interrupted) {
         if (!interrupted) {
             robot.turret.setTurret(Turret.TurretState.OFF, 0);
             robot.launcher.setActiveControl(false);
@@ -86,6 +85,6 @@ public class MovingAim extends CommandBase {
         MathFunctions.ShootingMath.PredictResult values = math.predict(robotPose, robotSpeed);
         robot.launcher.setFlywheel(values.flyWheelSpeed, true);
         robot.launcher.setHood(values.hoodAngle);
-        robot.turret.setTurret(ANGLE_CONTROL,values.turretAngle);
+        robot.turret.setTurret(ANGLE_CONTROL, values.turretAngle);
     }
 }
